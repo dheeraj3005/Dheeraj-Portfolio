2aa844c1c656f92329ba1d152cbc742ce604e670
dheeraj3005 <113555128+dheeraj3005@users.noreply.github.com>
2024-06-25 12:49:08 -0400

    backend link update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,7 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Send");
-    let response = await fetch("http://localhost:5001/contact", {
+    let response = await fetch("https://dheeraj-portfolio-3.onrender.com/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",

62aab99747cdcc6d87e17d3dde7842d2b43816aa
dheeraj3005 <113555128+dheeraj3005@users.noreply.github.com>
2025-02-06 14:44:19 -0500

    Update Banner.js - 2

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Dheeraj Lokesh`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software Developer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>I'm a Software Developer with 3+ years of experience.I am proficient in React, Javascript, Python & Java, Reachout to learn more!</p>
+                  <p>I'm a Software Developer/Engineer with 3+ years of experience. I am proficient in React, Javascript, Python & Java. Reachout to learn more!</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
